3e04b1a4006d23eab5a9b27b943c21f135011f07
MoonJuhan <codemike@naver.com>
2023-12-11 21:47:22 +0900

    feat: Add scroll to last func

--- index.js
+++ index.js
@@ -24,16 +24,71 @@ const init = async () => {
   }
 
   const titleElement = await getTitleElement()
+  const titleText = await titleElement.getText()
 
-  console.log(await titleElement.getText())
+  console.log(`Title: ${titleText}`)
 
   const targetElements = await titleElement.findElement(By.xpath('../../../..')).findElements(By.xpath('./*'))
 
+  let scrollElement
+
   for (let i = 0; i < targetElements.length; i += 1) {
-    const text = await targetElements[i].getText()
-    console.log(i, text)
+    if (!scrollElement) {
+      scrollElement = targetElements[i]
+    }
+
+    if (scrollElement) {
+      const scrollElementChildren = await scrollElement.findElements(By.xpath('./*'))
+      const children = await targetElements[i].findElements(By.xpath('./*'))
+
+      if (children.length > scrollElementChildren.length) {
+        scrollElement = targetElements[i]
+      }
+    }
   }
 
+  const myplacesElements = []
+
+  const getMyPlacesElements = async () => {
+    let prevChildrenLength = 0
+
+    const scrollToLast = async (repeatIndex = 0) => {
+      if (repeatIndex > 3) return
+
+      driver.actions().scroll(0, 0, 0, 9999, scrollElement).perform()
+      await sleep(1000)
+
+      const children = await scrollElement.findElements(By.xpath('./*'))
+
+      if (prevChildrenLength !== children.length) {
+        prevChildrenLength = children.length
+        await scrollToLast()
+        return
+      }
+
+      prevChildrenLength = children.length
+      await scrollToLast(repeatIndex + 1)
+    }
+
+    await scrollToLast()
+
+    console.log('Scroll To Last')
+
+    const children = await scrollElement.findElements(By.xpath('./*'))
+
+    for (let i = 0; i < children.length; i += 1) {
+      try {
+        const myPlaceNameElement = await children[i].findElement(By.css('.fontHeadlineSmall'))
+        const myPlaceName = await myPlaceNameElement.getText()
+
+        myplacesElements.push({ name: myPlaceName, element: children[i] })
+      } catch (error) {}
+    }
+  }
+
+  await getMyPlacesElements()
+
+  console.log(myplacesElements.length)
   driver.quit()
 }
 
